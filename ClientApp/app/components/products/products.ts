dcd7a76bc61029c0a85e5562d22bbef1847c78a8
Shaun Bai <ShaunBai@outlook.com>
2017-06-04 19:53:00 -0500

    added products page to navbar

---
+++ ClientApp/app/components/products/products.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'products',
+    templateUrl: './products.html'
+})
+export class ProductsComponent {
+    public currentProduct = 0;
+}
